7d56096ce27b8195437899508bae9d6f5fc11cc7
Shyam <gshyamprasad2001@gmail.com>
2025-03-08 17:41:11 +0530

    feat:added auth Middlewarwe

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,38 @@
 
 
+const jwt = require('jsonwebtoken');
 
+const User = require('../models/User')
+
+
+//Authentican Middleware
+
+const authMiddleware = async (req,resizeBy,next){
+    try {
+
+      const token = req.header('Authorization')?.replace('Bearer','') 
+      
+      if(!token){
+        return res.status(401).json({message:"Authentication Token required"})        
+      }
+
+      // Wee need to verify Token here 
+
+      const decoded = jwt.verify(token,process.env.JWT_SECRET)
+      console.log(decoded);
+      const user = await User.findById(decoded.userId)
+      if(!user){
+        res.status(401).json({message:"Invalid Token"})
+      }
+
+      req.user = user;
+      req.userId = user._id;
+
+      next()
+
+    } catch (error) {
+        res.status(401).json({message:"Authentication failed"})
+    }
+}
+
+module.exports = authMiddleware;
